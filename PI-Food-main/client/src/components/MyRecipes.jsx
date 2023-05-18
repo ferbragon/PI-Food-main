c036a1b4d853f6ca61a82ae025f64996e14e0811
ferbragon <nando.gon17@gmail.com>
2023-05-18 14:33:49 -0600

    Commit client

--- PI-Food-main/client/src/components/MyRecipes.jsx
+++ PI-Food-main/client/src/components/MyRecipes.jsx
@@ -1,9 +1,37 @@
-
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import Recipe from "../components/Recipe";
+import "../stylesheets/MyRecipes.css";
 
 const MyRecipes = () => {
+    const recipesInDataBase = useSelector(state => state.dbRecipes);
+
+    useEffect(() => {
+
+    }, [recipesInDataBase]);
+
     return(
         <div className="myRecipes">
             <h1>My Recipes</h1>
+            <div className="recipesFlex">
+            <NavLink to="/createNewRecipe">Create New Recipe</NavLink>
+            {
+                recipesInDataBase && recipesInDataBase.length > 0 && recipesInDataBase[0].message
+                ? <p>{recipesInDataBase[0].message}</p>
+                : recipesInDataBase && recipesInDataBase.map((recipe) => (
+                    <Recipe
+                        key={recipe.id}
+                        id={recipe.id}
+                        title={recipe.title}
+                        image={recipe.image}
+                        vegan={recipe.vegan}
+                        vegetarian={recipe.vegetarian}
+                        glutenFree={recipe.glutenFree}
+                        dietTags={recipe.dietTags}
+                    />))
+            }
+            </div>
         </div>
     )
 };
--- PI-Food-main/client/src/components/Recipe.jsx
+++ PI-Food-main/client/src/components/Recipe.jsx
@@ -2,11 +2,32 @@ import { NavLink } from "react-router-dom";
 import "../stylesheets/Recipe.css";
 
 const Recipe = ({ id , vegetarian, vegan, glutenFree, title, image, dietTags }) =>{
+    let diets = [];
+        if(vegetarian && dietTags.indexOf("vegetarian") === -1) diets.push("vegetarian");
+        if(vegan && dietTags.indexOf("vegan") === -1) diets.push("vegan");
+        if(glutenFree && dietTags.indexOf("gluten free") === -1) diets.push("gluten free");
+        if(dietTags) diets = [...diets,...dietTags];
     return(
-        <div className="recipe">
-            <NavLink>
-                <div className="label-1 left">Pescatarian</div>
-                <img className="recipe-img" src={image} alt={title} />
+        <div className="recipe" diets={diets} key={id}>
+            <NavLink to={`/:${id}`}>
+            <div className="corner-ribbon">
+            <img className="recipe-img" src={image} alt={title} />
+            <div className="diets">
+                {diets.length === 0 
+                    ? "View Detail" 
+                    : <ul>
+                        {diets.map((diet, index) => (
+                        <li key={index}>{diet}</li>
+                        ))}
+                    </ul>
+                }
+                </div>
+                <div>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
             </NavLink>
             <p>{title}</p>
         </div>
