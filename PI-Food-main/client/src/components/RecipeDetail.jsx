2332a9ca4cadd8711f9581c3408cbfee20063e2a
ferbragon <nando.gon17@gmail.com>
2023-05-19 15:00:17 -0600

    Commit client

--- PI-Food-main/client/src/components/RecipeDetail.jsx
+++ PI-Food-main/client/src/components/RecipeDetail.jsx
@@ -1,9 +1,68 @@
+import { useParams, NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+import StepByStep from "./StepByStep";
+//import "../stylesheets/RecipeDetail.css";
+import axios from "axios";
 
 
 const RecipeDetail = () => {
+
+    const { id } = useParams();
+
+    const [recipe, setRecipe] = useState({});
+    const [diets, setDiets] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { data } = await axios(`http://localhost:3001/recipes/${id}`);
+                setRecipe(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    
+        fetchData();
+    }, [id]);
+
+    useEffect(() => {
+        let newDiets = [];
+        if(recipe.vegetarian && recipe.dietTags.indexOf("vegetarian") === -1) newDiets.push("vegetarian");
+        if(recipe.vegan && recipe.dietTags.indexOf("vegan") === -1) newDiets.push("vegan");
+        if(recipe.glutenFree && recipe.dietTags.indexOf("gluten free") === -1) newDiets.push("gluten free");
+        if(recipe.dietTags) newDiets = [...newDiets,...recipe.dietTags];
+
+        setDiets(newDiets);
+    }, [recipe]);
+
+    //Capitalize first letter 
+    const capitalizeFirstLetter = (string) =>{
+        let devide = string.split(" ");
+        let upperCase = devide.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+        let join = upperCase.join(" ");
+        return join;
+    };
+
+    console.log(recipe.analyzedInstructions);
+
     return(
         <div className="recipeDetail">
+            <button className="button-detail-close">
+              <NavLink to="/home">X</NavLink>
+            </button>
+            <img src={recipe.image} alt={recipe.title}/>
             <h1>Detail Recipe</h1>
+            <h2>{recipe.id}</h2>
+            <h1>{recipe.title}</h1>
+            <h5>Health Score: {recipe.healthScore}</h5>
+            {/*<div dangerouslySetInnerHTML={{ __html: recipe.summary }} />*/}
+            <ul>
+                {diets.map((diet, index) => (
+                    <li key={index}>{capitalizeFirstLetter(diet)}</li>
+                ))}
+            </ul>
+            <span>Instructions:</span>{recipe.instructions}
+            {recipe.analyzedInstructions && <StepByStep stepByStep={recipe.analyzedInstructions}/>}
         </div>
     )
 };
--- PI-Food-main/client/src/components/SearchBar.jsx
+++ PI-Food-main/client/src/components/SearchBar.jsx
@@ -1,9 +1,58 @@
-
+import Search from "./Search";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchByName, orderRecipes, filterRecipes } from "../redux/actions";
 
 const SearchBar = () => {
+
+    //Global State
+    const diets = useSelector(state => state.diets);
+
+    //Action call for recipe name
+    const dispatch = useDispatch();
+
+    const onSearch = async (title) =>{
+    await dispatch(fetchByName(title));
+    };
+
+    //Order and Filter
+    const handleOrder = (event) => {
+        dispatch(orderRecipes(event.target.value));
+    };
+
+    const handleFilter = (event) => {
+        dispatch(filterRecipes(event.target.value));
+    };
+
+    //Capitalize first letter 
+    const capitalizeFirstLetter = (string) =>{
+        let devide = string.split(" ");
+        let upperCase = devide.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+        let join = upperCase.join(" ");
+        return join;
+    };
+
     return(
         <div>
             <h1>SearchBar</h1>
+            <select onChange={handleOrder} >
+                <option className="option" value="A">Ascendent</option>
+                <option className="option" value="D">Descendent</option>
+                <option className="option" value="a">A to Z</option>
+                <option className="option" value="z">Z to A</option>
+                <option className="option" value="AHS">Ascendent Health Score</option>
+                <option className="option" value="DHS">Descendent Health Score</option>
+            </select>
+            <select onChange={handleFilter}>
+                <option className="option" value="All">All Recipes</option>
+                <option className="option" value="MyRecipes">My recipes</option>
+                <option className="option" value="Spoonacular">Spoonacular recipes</option>
+                {diets.map((diet) => {
+                    return(
+                        <option className="option" value={diet.name} key={diet.id}>{capitalizeFirstLetter(diet.name)}</option>
+                    )
+                })}
+        </select>
+            <Search onSearch={onSearch} />
         </div>
     )
 };
