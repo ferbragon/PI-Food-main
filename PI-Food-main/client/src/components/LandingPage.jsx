2140ec12e62702af97f517f5f794dce14e5b2b7f
ferbragon <nando.gon17@gmail.com>
2023-05-27 11:25:28 -0600

    Commit client

--- PI-Food-main/client/src/components/LandingPage.jsx
+++ PI-Food-main/client/src/components/LandingPage.jsx
@@ -1,22 +1,49 @@
 import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import "../stylesheets/LandingPage.css";
-import React from "react";
+import imageBackground from "../assets/foodImageLanding.png";
+import React, { useState, useEffect }from "react";
+import SwitchButton from "./SwitchButton";
+import { login } from "../redux/actions";
+import Loading from "./Loading";
 
 const LandingPage = ({ handleRecipes })=> {
 
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    //Local state
+    const [loading, setLoading] = useState(false);
+    //Global State
+    const enter = useSelector((state) => state.login);
 
-    const handleEnterButton = () =>{
-        handleRecipes();
-        navigate("/home");
+    useEffect(() => {
+        if (!enter) {
+            navigate('/');
+        }
+    }, [enter, navigate]);
+
+     const handleEnterButton = () => {
+        if(!enter){
+            setLoading(true);
+            dispatch(login(!enter));
+            handleRecipes();
+            setTimeout(() => {
+                navigate("/home");
+                setLoading(false);
+            }, 2500);
+        }else{
+            navigate("/");
+        };
     }
 
     return(
         <div className="landingPage">
-            <h1>Landing Page</h1>
-            <button onClick={handleEnterButton} className="landingPage-button">
-                Enter
-            </button>
+            <h1 className="welcome" >Let's do recipes!</h1>
+            <SwitchButton className="enterButton" handleEnterButton={handleEnterButton} />
+            {loading && <Loading />}
+            <div className="hola">
+            <img src={imageBackground} className={!enter ? "imageBackground" : "imageBackgroundAfter"} alt="food background"/>
+            </div>
         </div>
     )
 };
--- PI-Food-main/client/src/components/MyRecipes.jsx
+++ PI-Food-main/client/src/components/MyRecipes.jsx
@@ -1,36 +1,42 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { NavLink, /*useNavigate*/ } from "react-router-dom";
 import Recipe from "../components/Recipe";
 import "../stylesheets/MyRecipes.css";
 
-const MyRecipes = () => {
+
+const MyRecipes = ({ openRecipeDetail, openCreateNewRecipe, searchMode }) => {
+
+
     const recipesInDataBase = useSelector(state => state.dbRecipes);
+    
+    
 
     useEffect(() => {
 
     }, [recipesInDataBase]);
 
     return(
-        <div className="myRecipes">
-            <h1>My Recipes</h1>
+        <div className={!searchMode ? "myRecipes" : "myRecipesSearchMode"}>
+             <h1 className="titleMyRecipes">My Recipes</h1>
             <div className="recipesFlex">
-            <NavLink to="/createNewRecipe">Create New Recipe</NavLink>
-            {
-                recipesInDataBase && recipesInDataBase.length > 0 && recipesInDataBase[0].message
-                ? <p>{recipesInDataBase[0].message}</p>
-                : recipesInDataBase && recipesInDataBase.map((recipe) => (
-                    <Recipe
-                        key={recipe.id}
-                        id={recipe.id}
-                        title={recipe.title}
-                        image={recipe.image}
-                        vegan={recipe.vegan}
-                        vegetarian={recipe.vegetarian}
-                        glutenFree={recipe.glutenFree}
-                        dietTags={recipe.dietTags}
-                    />))
-            }
+            <NavLink className="createNewRecipe" onClick={openCreateNewRecipe}>+</NavLink>
+                {recipesInDataBase &&
+                    recipesInDataBase.map((recipe) => (
+                        <Recipe
+                            key={recipe.id}
+                            db={recipe.db}
+                            id={recipe.id}
+                            title={recipe.title}
+                            image={recipe.image}
+                            vegan={recipe.vegan}
+                            vegetarian={recipe.vegetarian}
+                            glutenFree={recipe.glutenFree}
+                            dietTags={recipe.dietTags}
+                            openRecipeDetail={openRecipeDetail}
+                        />
+                    ))
+                }
             </div>
         </div>
     )
